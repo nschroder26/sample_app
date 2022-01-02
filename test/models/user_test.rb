96fc54068c280323e19de4801a3b8dad86a6abff
Nicholas Schroder <nrs4492@hotmail.com>
2022-01-02 15:28:41 +0000

    add index to db. add uniqueness check for email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -45,5 +45,17 @@ class UserTest < ActiveSupport::TestCase
     end 
   end
   
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+end
+
+  test "email addresses should be saved as lowercase" do 
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
   
 end
