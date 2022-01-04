4181e927c873ffb337ad3cbea584dc6cb95d0c36
Nicholas Schroder <nrs4492@hotmail.com>
2022-01-04 17:51:56 +0000

    add 2 tests for current user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,11 +3,15 @@ module SessionsHelper
   #logs in user
   def log_in(user)
     session[:user_id] = user.id
+    session[:session_token] = user.session_token
   end
   
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
+      user = User.find_by(id: user_id)
+      if user && session[:session_token] == user.session_token
+        @current_user = user
+      end
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
