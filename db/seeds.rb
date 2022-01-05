61f67e2743cc2727a468f7f5c042174aae002f3d
Nicholas Schroder <nrs4492@hotmail.com>
2022-01-05 19:54:23 +0000

    add admin param to db, set default to false

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 #create a main sample user
 
 User.create!(name: "Example User", email:"example@railstutorial.org",
-  password: "foobar", password_confirmation: "foobar")
+  password: "foobar", password_confirmation: "foobar", admin: true)
 
 99.times do |n|
   name = Faker::Name.name
