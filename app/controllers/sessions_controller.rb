48dba0e30bbd3b427c4374529f2c47cc73ad4627
Nicholas Schroder <nrs4492@hotmail.com>
2022-01-07 00:48:20 +0000

    fix failing test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase) 
     if @user&.authenticate(params[:session][:password])
-      if user.activated?
+      if @user.activated?
         forwarding_url = session[:forwarding_url]
         reset_session
         params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
