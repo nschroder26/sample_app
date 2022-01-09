77670ae85de1d871ae6ceb783e5063701018488a
Nicholas Schroder <nrs4492@hotmail.com>
2022-01-09 15:46:13 +0000

    add follow then unfollow test. add follow, unfollow, and following? methods to user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -81,4 +81,17 @@ end
       @user.destroy
     end
   end
+  
+  test "should follow and unfollow a user" do
+    michael = users(:michael)
+    archer = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+    # Users can't follow themselves.
+    michael.follow(michael)
+    assert_not michael.following?(michael)
+  end
 end
