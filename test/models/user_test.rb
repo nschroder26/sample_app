f799a93d6094ab53819adc57e8b9523a9b5eb3da
Nicholas Schroder <nrs4492@hotmail.com>
2022-01-09 15:53:30 +0000

    add followers logic

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -88,6 +88,7 @@ end
     assert_not michael.following?(archer)
     michael.follow(archer)
     assert michael.following?(archer)
+    assert archer.followers.include?(michael)
     michael.unfollow(archer)
     assert_not michael.following?(archer)
     # Users can't follow themselves.
