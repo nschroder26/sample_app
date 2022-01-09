711b1b9293eda9f8b9769a65e941db202281850b
Nicholas Schroder <nrs4492@hotmail.com>
2022-01-09 18:45:22 +0000

    refine status feed. add test to check for duplicate self posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -67,7 +67,10 @@ class User < ApplicationRecord
   end
   
   def feed
-    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+    part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
+    Micropost.left_outer_joins(user: :followers)
+      .where(part_of_feed, { id: id }).distinct
+      .includes(:user, image_attachment: :blob)
   end
   
   # Follows a user.
